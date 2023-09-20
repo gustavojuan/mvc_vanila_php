04d8c27b50fe5ec3cf4d7e7ef3ddc5633a4ab36e
Gustavo Juan <gustavojuandev@gmail.com>
2023-09-20 14:00:38 +0200

    Basic routing onprocess

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -12,10 +12,10 @@
                         <a href="/"
                            class="<?= urlIs("/") ? "bg-gray-900 text-white" : "text-gray-300"; ?> text-gray-300 rounded-md px-3 py-2 text-sm font-medium"
                         >Home</a>
-                        <a href="/about.php"
+                        <a href="/about"
                            class="<?= urlIs("/about.php") ? "bg-gray-900 text-white" : "text-gray-300"; ?> text-gray-300 rounded-md px-3 py-2 text-sm font-medium">About
                             us</a>
-                        <a href="/contact.php"
+                        <a href="/contact"
                            class="<?= urlIs("/contact.php") ? "bg-gray-900 text-white" : "text-gray-300"; ?> text-gray-300 rounded-md px-3 py-2 text-sm font-medium">Contact</a>
 
 
