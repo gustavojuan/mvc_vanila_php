225f0ed5e9ec64aed335edfb861f000a6ab8fba6
Gustavo Juan <gustavojuandev@gmail.com>
2023-09-20 13:51:06 +0200

    SuperGLobals and functions

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -9,12 +9,14 @@
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="/index.php" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                        <a href="/"
+                           class="<?= urlIs("/") ? "bg-gray-900 text-white" : "text-gray-300"; ?> text-gray-300 rounded-md px-3 py-2 text-sm font-medium"
                         >Home</a>
                         <a href="/about.php"
-                           class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About us</a>
+                           class="<?= urlIs("/about.php") ? "bg-gray-900 text-white" : "text-gray-300"; ?> text-gray-300 rounded-md px-3 py-2 text-sm font-medium">About
+                            us</a>
                         <a href="/contact.php"
-                           class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+                           class="<?= urlIs("/contact.php") ? "bg-gray-900 text-white" : "text-gray-300"; ?> text-gray-300 rounded-md px-3 py-2 text-sm font-medium">Contact</a>
 
 
                     </div>
