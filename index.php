607f0811e7acc204d119f9974b7d302deafd01d4
Gustavo Juan <gustavojuandev@gmail.com>
2023-09-21 10:23:11 +0200

    Environments and Configuration Flexibility

--- index.php
+++ index.php
@@ -3,10 +3,12 @@ require 'functions.php';
 require 'Database.php';
 //require 'router.php';
 
+$config = require 'config.php';
 
-// Connect to DB and execure a query
-$db = new Database();
-$posts = $db->query("SELECT * FROM posts where id > 0")->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database($config['database']);
+
+$posts = $db->query("SELECT * FROM posts where id > 0")->fetchAll();
 dd($posts);
-$posts = $db->query("SELECT * FROM posts where id > 0")->fetch(PDO::FETCH_ASSOC);
+
+$posts = $db->query("SELECT * FROM posts where id > 0")->fetch();
 dd($posts);
