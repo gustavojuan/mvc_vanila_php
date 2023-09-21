b4e865676690825a937ef2da1898eeb6a7661e33
Gustavo Juan <gustavojuandev@gmail.com>
2023-09-21 12:47:41 +0200

    Render the Notes and Note Page

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
+
 require 'functions.php';
 require 'Database.php';
-//require 'router.php';
-
-$config = require 'config.php';
-
-$db = new Database($config['database']);
-
-// Injection
-// http://demo.test/?id=1 drop table users
-$id = $_GET['id'];
-//never pass user input to a query
-$query =  "SELECT * FROM posts where id = :id";
-
-$posts = $db->query($query,[':id' => $id])->fetch();
-dd($posts);
-
+require 'router.php';
