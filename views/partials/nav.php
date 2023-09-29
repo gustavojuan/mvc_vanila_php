4ee5207e19c1f90cd462339564966b28e3e9b637
Gustavo Juan <gustavojuandev@gmail.com>
2023-09-29 08:48:59 +0200

    Login and restricted views

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -10,13 +10,16 @@
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                         <a href="/"
-                           class="<?= urlIs("/") ? "bg-gray-900 text-white" : "text-gray-300"; ?> text-gray-300 rounded-md px-3 py-2 text-sm font-medium"
-                        >Home</a>
+                           class="<?= urlIs("/") ? "bg-gray-900 text-white" : "text-gray-300"; ?> text-gray-300 rounded-md px-3 py-2 text-sm font-medium">Home</a>
                         <a href="/about"
                            class="<?= urlIs("/about") ? "bg-gray-900 text-white" : "text-gray-300"; ?> text-gray-300 rounded-md px-3 py-2 text-sm font-medium">About
                             us</a>
-                        <a href="/notes"
-                           class="<?= urlIs("/notes") ? "bg-gray-900 text-white" : "text-gray-300"; ?> text-gray-300 rounded-md px-3 py-2 text-sm font-medium">Notes</a>
+
+                        <?php if ($_SESSION['user'] ?? false) : ?>
+                            <a href="/notes"
+                               class="<?= urlIs("/notes") ? "bg-gray-900 text-white" : "text-gray-300"; ?> text-gray-300 rounded-md px-3 py-2 text-sm font-medium">Notes</a>
+
+                        <?php endif ?>
                         <a href="/contact"
                            class="<?= urlIs("/contact") ? "bg-gray-900 text-white" : "text-gray-300"; ?> text-gray-300 rounded-md px-3 py-2 text-sm font-medium">Contact</a>
 
@@ -43,24 +46,26 @@
                             <?php if ($_SESSION['user'] ?? false): ?>
 
                                 <div class="flex">
-                                <button type="button"
-                                        class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                        id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                    <span class="absolute -inset-1.5"></span>
-                                    <span class="sr-only">Open user menu</span>
+                                    <button type="button"
+                                            class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                            id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                        <span class="absolute -inset-1.5"></span>
+                                        <span class="sr-only">Open user menu</span>
 
-                                    <img class="h-8 w-8 rounded-full"
-                                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                         alt="">
+                                        <img class="h-8 w-8 rounded-full"
+                                             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                             alt="">
 
-                                </button>
-                                <div class="ml-3">
-                                    <form method="POST" action="/session">
-                                        <input type="hidden" name="_method" value="DELETE"/>
+                                    </button>
+                                    <div class="ml-3">
+                                        <form method="POST" action="/session">
+                                            <input type="hidden" name="_method" value="DELETE"/>
 
-                                        <button class="text-gray-300 rounded-md px-3 py-2 text-sm font-medium">Log Out</button>
-                                    </form>
-                                </div>
+                                            <button class="text-gray-300 rounded-md px-3 py-2 text-sm font-medium">Log
+                                                Out
+                                            </button>
+                                        </form>
+                                    </div>
                                 </div>
 
                             <?php else: ?>
