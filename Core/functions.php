00d6d3e29fc2893a626589acb8e5ee476a79f658
Gustavo Juan <gustavojuandev@gmail.com>
2023-10-04 12:35:39 +0200

    Flash Old Form Data to the Session

--- Core/functions.php
+++ Core/functions.php
@@ -52,3 +52,10 @@ function redirect($path)
     header('Location: ' . $path);
     exit();
 }
+
+
+
+function old($key ,$default = '')
+{
+     return  \Core\Session::get('old')[$key] ?? $default ;
+}
